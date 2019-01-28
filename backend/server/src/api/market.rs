b2b742e3c5c51a60d12e703f7fd03f7d30d5fe47
Henry Zimmerman <zimhen7@gmail.com>
2019-01-28 14:00:13 -0500

    withdraw abstracted into its own function

--- backend/server/src/api/market.rs
+++ backend/server/src/api/market.rs
@@ -11,6 +11,7 @@ use uuid::Uuid;
 use db::funds::Funds;
 use crate::error::Error;
 use crate::util;
+use warp::Rejection;
 
 pub fn market_api(s: &State) -> BoxedFilter<(impl Reply,)> {
 
@@ -56,19 +57,7 @@ pub fn market_api(s: &State) -> BoxedFilter<(impl Reply,)> {
         .and(json_body_filter(10))
         .and(user_filter(s))
         .and(s.db.clone())
-        .and_then(|quantity: f64, user_uuid: Uuid, conn: PooledConn| {
-            if quantity < 0.0 {
-                // No negative numbers allowed
-                Error::BadRequest.reject_result()
-            } else {
-                let negative_quantity = 0.0 - quantity;
-                Funds::transact_funds(user_uuid, negative_quantity, &conn)
-                    .map_err(Error::from)
-                    .map_err(Error::reject)
-                    .map(|funds: Funds| funds.quantity) // maps it to just a f64
-                    .map(util::json)
-            }
-        });
+        .and_then(withdraw_funds);
 
     // balance
     let balance = path!("balance")
@@ -111,4 +100,37 @@ pub fn market_api(s: &State) -> BoxedFilter<(impl Reply,)> {
         )
         .boxed()
 
+}
+
+/// Withdraws funds from a user's Funds.
+///
+/// It will first check that the provided quantity is non-negative
+/// It will then check that if by withdrawing funds, the user will not have a negative balance.
+///
+/// If those checks are OK, then the function will deduct the quantity from the users account.
+fn withdraw_funds(quantity: f64, user_uuid: Uuid, conn: PooledConn) -> Result<impl Reply, Rejection> {
+    if quantity < 0.0 {
+        // No negative numbers allowed.
+        Error::BadRequest.reject_result()
+    } else {
+        let negative_quantity = 0.0 - quantity;
+        // Check if withdrawing will give a negative balance.
+        let _ = Funds::funds(user_uuid, &conn)
+            .map_err(Error::from)
+            .and_then(|current_funds| {
+                if current_funds.quantity + negative_quantity < 0.0 {
+                    Err(Error::BadRequest)
+                } else {
+                    Ok(())
+                }
+            })
+            .map_err(Error::reject)?;
+
+        // Withdraw the funds from the user's balance.
+        Funds::transact_funds(user_uuid, negative_quantity, &conn)
+            .map(|funds: Funds| funds.quantity) // maps it to just a f64
+            .map(util::json)
+            .map_err(Error::from)
+            .map_err(Error::reject)
+    }
 }
