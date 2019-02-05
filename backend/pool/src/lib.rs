6dbee7fe18e047706248d4f72e3b8064585f2254
Henry Zimmerman <zimhen7@gmail.com>
2019-02-04 20:18:05 -0500

    set up clap

--- backend/pool/src/lib.rs
+++ backend/pool/src/lib.rs
@@ -1,5 +1,7 @@
 use diesel::{pg::PgConnection, r2d2::ConnectionManager, Connection};
 use r2d2::{Pool as R2D2Pool, PooledConnection};
+use std::time::Duration;
+use apply::Apply;
 
 pub const DATABASE_URL: &'static str = env!("DATABASE_URL");
 
@@ -7,10 +9,40 @@ pub const DATABASE_URL: &'static str = env!("DATABASE_URL");
 pub type Pool = R2D2Pool<ConnectionManager<PgConnection>>;
 pub type PooledConn = PooledConnection<ConnectionManager<PgConnection>>;
 
+
+/// Corfiguration object for the pool.
+#[derive(Default, Debug)]
+pub struct PoolConfig {
+    pub max_connections: Option<u32>,
+    pub min_connections: Option<u32>,
+    /// Max lifetime in minutes.
+    pub max_lifetime: Option<Duration>,
+    pub timeout: Duration
+}
+
 /// Initializes the pool.
-pub fn init_pool(db_url: &str) -> Pool {
+pub fn init_pool(db_url: &str, conf: PoolConfig) -> Pool {
     let manager = ConnectionManager::<PgConnection>::new(db_url);
-    r2d2::Pool::new(manager).expect("db pool")
+    let builder = r2d2::Pool::builder()
+        .apply(|builder| {
+            if let Some(max_size) = conf.max_connections {
+               builder.max_size(max_size)
+            } else {
+                builder
+            }
+        })
+        .apply(|builder| {
+            builder.min_idle(conf.min_connections)
+        })
+        .apply(|builder| {
+            if let Some(max_lifetime) = conf.max_lifetime {
+                builder.max_lifetime(Some(max_lifetime))
+            } else {
+                builder
+            }
+        });
+
+    builder.build(manager).expect("Could not initialize DB Pool")
 }
 
 pub fn create_single_connection(db_url: &str) -> PgConnection {
