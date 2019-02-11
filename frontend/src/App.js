dfbe6aec99dca49eba2c4960afe5fbe32e5ce866
Beltran Caliz <bjc8008@rit.edu>
2019-02-11 11:19:35 -0500

    Frontend project structure and navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from './app/home/components/Home.js';
+import Login from './app/auth/components/Login.js';
 import './App.css';
+
+const Main = () => (
+  <main>
+    <Switch>
+      <Route exact path='/' component={Home}/>
+      <Route exact path='/login' component={Login}/>
+    </Switch>
+  </main>
+);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            SWEN 344 Project
-          </p>
-
-        </header>
-      </div>
+      <Main />
     );
   }
 }
