4f00090f8af074c5f96e535f93fb9351edd2a67e
Beltran Caliz <bjc8008@rit.edu>
2019-02-11 11:22:49 -0500

    Frontend project structure and navigation (#54)

---
+++ frontend/src/app/auth/components/Login.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../../../App.css';
+
+export default class Login extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Login Page
+          </p>
+
+        </header>
+      </div>
+    );
+    
+  }
+
+}
---
+++ frontend/src/app/home/components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../../../App.css';
+
+export default class Home extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Home Page
+          </p>
+
+        </header>
+      </div>
+    );
+
+  }
+
+}
