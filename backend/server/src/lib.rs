9af072b0153935a6a75293e91c5153e242d8f7a0
JKingSE <jfk1307@g.rit.edu>
2019-03-02 18:04:08 -0500

    Initial selenium commit, webdriver is downloaded and dependencies are added to Rust.  Need to check that Rust config is correct, then tests need to be written

--- backend/server/src/lib.rs
+++ backend/server/src/lib.rs
@@ -29,6 +29,8 @@ use crate::{
 use env_logger::Builder as LoggerBuilder;
 use log::{info, LevelFilter};
 
+extern crate selenium_rs;
+
 /// parses the command line arguments and starts the server.
 pub fn start() {
     LoggerBuilder::new().filter_level(LevelFilter::Info).init();
