7331032bb91559a2351d3be0ee0f6490a99791af
Henry Zimmerman <hgzimmerman@users.noreply.github.com>
2019-04-01 10:24:05 -0400

    backup the frontend r1 release (#135)

---
+++ frontend_backup/src/app/stocks/components/StockChart.js
@@ -0,0 +1,50 @@
+import React from "react";
+import CanvasJSReact from '../../../assets/canvasjs.react';
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
+export default class StockChart extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      data: this.props.data
+    }
+  }
+
+  feedData(){
+    let arr = [];
+    this.state.data.map((item) => {
+      arr.push({x: new Date(parseInt(item.date.substr(0,4)), parseInt(item.date.substr(5,2)-1), parseInt(item.date.substr(8,2))), y: item.close})
+    });
+    return arr;
+  }
+
+
+  render() {
+    const dataPoints = this.feedData();
+		const options = {
+			animationEnabled: true,
+			title: {
+				text: `${this.props.stock} Interactive Chart (Month)`
+			},
+			axisY: {
+				title: "Closed Price",
+				includeZero: false,
+				prefix: "$"
+			},
+			data: [{
+				type: "splineArea",
+				xValueFormatString: "MM DD",
+				yValueFormatString: "#,$##0.##",
+				showInLegend: false,
+				legendText: "Date",
+				dataPoints: dataPoints
+			}]
+		}
+
+		return (
+		<div>
+			<CanvasJSChart options = {options} />
+		</div>
+		);
+	}
+}
