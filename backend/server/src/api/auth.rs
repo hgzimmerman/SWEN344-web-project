c55877d8ca47a34c1543f4f1d9115ddca05cca47
Henry Zimmerman <hgzimmerman@users.noreply.github.com>
2019-04-13 07:02:05 -0400

    Misc twitter cleanup (#142)
    * add more fields to twitter, return tweet from post tweet route
    
    * remove dead, commented-out code

--- backend/server/src/api/auth.rs
+++ backend/server/src/api/auth.rs
@@ -190,62 +190,6 @@ fn login_template_render(jwt: String) -> String {
 }
 
 
-/// Shim for the get_user_id_from_facebook function.
-/// The shim allows tests to always have the auth process succeed succeed.
-//fn get_user_id(
-//    request: LoginRequest,
-//    client: HttpsClient,
-//) -> impl Future<Item = String, Error = Error> {
-//    // If this runs in a test environment, it will work without question.
-//    // Otherwise, it will attempt to acquire the user_id from facebook.
-//    use futures::future::Either;
-//    info!("Getting user id from oauth provider");
-//    let oauth_token = &request.oauth_token;
-//    if cfg!(test) {
-//        futures::future::ok::<String, Error>(TEST_CLIENT_ID.to_string()).apply(Either::A) // Automatic user login for testing
-//    } else {
-//        get_user_id_from_facebook(oauth_token, client).apply(Either::B) // Await the response
-//    }
-//}
-
-/// Gets the user id from facebook
-///
-/// # Arguments
-/// * oauth_token - The string representing the oauth access token granted from facebook.
-/// * client - The https client used to make the request.
-// TODO verify that this works.
-//fn get_user_id_from_facebook(
-//    oauth_token: &str,
-//    client: HttpsClient,
-//) -> impl Future<Item = String, Error = Error> {
-//    info!("Making request to Facebook to get the user_id");
-//    let uri: Uri = format!("https://graph.facebook.com/me?access_token={}", oauth_token)
-//        .parse()
-//        .unwrap();
-//    client
-//        .get(uri.clone())
-//        .map_err(move |_| Error::DependentConnectionFailed {
-//            url: uri.to_string(),
-//        })
-//        .and_then(|res| {
-//            if res.status().is_client_error() {
-//                Err(
-//                    Error::not_authorized("Bad OAuth token"))
-//            } else {
-//                Ok(res)
-//            }
-//        })
-//        .and_then(|res| {
-//            res.into_body()
-//                .concat2()
-//                .map_err(|_| Error::internal_server_error_empty()) // Await the whole body
-//        })
-//        .map(|chunk: Chunk| -> String {
-//            let v = chunk.to_vec();
-//            String::from_utf8_lossy(&v).to_string()
-//        })
-//}
-
 /// Gets a user, and if the user doesn't exist yet, create one and return it anyway.
 ///
 /// # Arguments
@@ -283,39 +227,3 @@ fn get_or_create_user(
                 .map(|a| a)
         })
 }
-//
-//#[cfg(test)]
-//mod test {
-//    use super::*;
-//    use crate::{state::State, testing_fixtures::user::UserFixture};
-//    use pool::Pool;
-//    use testing_common::setup::setup_warp;
-//    use std::option::Option;
-//
-//    use crate::testing_fixtures::util::{deserialize, deserialize_string};
-//
-//    use authorization::{AUTHORIZATION_HEADER_KEY, BEARER};
-
-//    #[test]
-//    fn login_works() {
-//        setup_warp(|_fixture: &UserFixture, pool: Pool| {
-//            let secret = Secret::new("test");
-//            let s = State::testing_init(pool, secret);
-//            let filter = auth_api(&s);
-//
-//            let login = LoginRequest {
-//                oauth_token: "Test Garbage because we don't want to have the tests depend on FB"
-//                    .to_string(),
-//            };
-//            let resp = warp::test::request()
-//                .method("POST")
-//                .path("/auth/login")
-//                .json(&login)
-//                .header("content-length", "300")
-//                .reply(&filter);
-//
-//            assert_eq!(resp.status(), 200)
-//        });
-//    }
-
-//}
