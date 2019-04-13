7098a529d24b5c9c514415890cf66113eb7699b3
Henry Zimmerman <zimhen7@gmail.com>
2019-04-13 07:57:59 -0400

    make the start function more minimal by moving config parsing out of it

--- backend/server/src/main.rs
+++ backend/server/src/main.rs
@@ -1,7 +1,12 @@
 //! Binary for Server.
 use server::start;
+use server::Config;
+use log::LevelFilter;
+use env_logger::Builder as LoggerBuilder;
 
 /// Simple shell around starting the server.
 fn main() {
-    start()
+    LoggerBuilder::new().filter_level(LevelFilter::Info).init();
+    let config = Config::parse_command_line_arguments();
+    start(config)
 }
