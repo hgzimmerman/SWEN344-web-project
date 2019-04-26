9fd442977144d8b1538a2d4d98eeae4f41a801c8
Henry Zimmerman <zimhen7@gmail.com>
2019-04-26 07:25:47 -0400

    implemented frontend weather

--- frontend/src/app/weather/containers/Weather.js
+++ frontend/src/app/weather/containers/Weather.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import WeatherTable from '../components/WeatherTable.js';
 import {authenticatedFetchDe} from "../../../config/auth";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
 
 export default class Weather extends React.Component {
   constructor(props) {
@@ -9,22 +11,30 @@ export default class Weather extends React.Component {
       weather: null,
       isLoading: true,
       zipCode: '14623',
-      error: false
+      newZipCode: '',
+      error: null
     };
     this.getWeather = this.getWeather.bind(this);
+    this.handleOnChangeNewZip = this.handleOnChangeNewZip.bind(this);
+    this.setZipCode = this.setZipCode.bind(this);
+    this.handleKeyPressNewZip = this.handleKeyPressNewZip.bind(this);
   }
 
   componentDidMount() {
     this.getZipCode().then(() => this.getWeather(this.state.zipCode));
   }
 
+  /**
+   * Gets the zip code from the backend.
+   * Will default to the rochester zip code if the user hasn't set their zip code yet.
+   */
   getZipCode() {
     const url = "/api/user/zip";
     return authenticatedFetchDe(url)
       .then(response => {
         const defaultZipCode = "14623";
         if (response !== null && response !== undefined) {
-          this.setState({zipCode: response})
+          this.setState({zipCode: response, error: null})
         } else {
           this.setState({zipCode: defaultZipCode})
         }
@@ -57,15 +67,121 @@ export default class Weather extends React.Component {
         });
   }
 
-  render() {
-    return(
-      <WeatherTable
-        isLoading={this.state.isLoading}
-        weather={this.state.weather}
-        error={this.state.error}
-        getWeather={this.getWeather}
-      />
-    );
+  /**
+   * Handle changing text for the new zip text field
+   */
+  handleOnChangeNewZip(event) {
+      this.setState({
+        newZipCode: event.target.value,
+      });
+  }
+  /**
+   * Handle the Enter handling for the new zip text field
+   */
+  handleKeyPressNewZip(event) {
+     switch (event.key) {
+        case 'Enter':
+          console.log("enter pressed");
+          this.setZipCode().then(() => this.getWeather(this.state.zipCode));
+          break;
+        default:
+          break;
+    }
   }
 
+  /**
+   * Sets the zip code on the backend
+   * It uses the newZipCode state value to set the backend, and changes the zipcode state value in response.
+   */
+  setZipCode() {
+    const url = "/api/user/zip";
+
+    const extras = {method: "PUT", body: JSON.stringify(this.state.newZipCode)};
+    return authenticatedFetchDe(url, extras)
+      .then(response => {
+        this.setState({zipCode: response, error: null})
+      })
+      .catch(error => {
+        this.setState({error})
+      })
+  }
+
+  render() {
+    return(
+
+      <div className="App">
+        <h4> Current Zip Code: {this.state.zipCode} </h4>
+
+        <div style={styles.searchBar}>
+          <TextField
+            value={this.state.newZipCode}
+            id="outlined-with-placeholder"
+            label="Zip Code"
+            placeholder="ZIP Code..."
+            margin="normal"
+            variant="outlined"
+            onChange={this.handleOnChangeNewZip}
+            onKeyPress={this.handleKeyPressNewZip}
+            style={{width: '60%'}}
+          />
+        </div>
+        <Button
+          onClick={() => this.setZipCode().then(() => this.getWeather(this.state.zipCode))}
+          variant="contained"
+          style={styles.button}
+        >
+          Change Zip Code
+        </Button>
+        <div style={{textAlign: 'center'}} >
+
+        </div>
+        <div>
+          {
+            (!this.state.isLoading)
+              ? (this.state.error)
+                ? <p>
+                    Zip Code
+                    <span style={styles.bold}> "{this.state.zipCode}" </span>
+                    does not exist
+                  </p>
+                : (this.state.weather !== undefined)
+                  ? <div>
+                      <WeatherTable
+                        weather={this.state.weather}
+                      />
+                    </div>
+                  : <div><p>Enter a Zip Code for Weather</p></div>
+                : <div><p>Weather Loading...</p></div>
+          }
+        </div>
+      </div>
+    );
+  }
 }
+
+const styles = {
+  root: {
+    width: '100%',
+    marginTop: 20,
+    overflowX: 'auto',
+  },
+  button: {
+    backgroundColor: '#00A6DD',
+    color: 'white',
+    height: 50,
+    width: 200
+  },
+  table: {
+    minWidth: 700,
+  },
+  panel: {
+    padding: 30
+  },
+  searchBar: {
+    marginTop: 40,
+    marginBottom: 10
+  },
+  bold: {
+    fontWeight: 'bold'
+  }
+};
